d290eed834222613ea2ee212d6a7cf9f68e6ba70
Misterpeka <37779450+Misterpeka@users.noreply.github.com>
2018-03-25 22:18:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.on('ready', function () {
-    console.log("Je suis connecté !")
-  })
-
-  bot.on('message', message => {
-    if (message.content === 'ping') {
-      message.reply('pong !')
-    }
-  })
   
 bot.login(process.env.TOKEN)
