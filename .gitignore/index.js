e95fb088382fdd48343c03b61765aac8343bfa6e
Misterpeka <37779450+Misterpeka@users.noreply.github.com>
2018-03-25 21:14:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,4 +2,4 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
-bot.login('NDI0OTIxNTI4MjMzMzYxNDE4.DZlyrA.fLNk-Ivo275J69Q0-vCPh6gNSc0')
+bot.login('NDI0OTIxNTI4MjMzMzYxNDE4.DZmGMg.HJ7wze7HSU_ZoSPgYL56DzE5Ji0')
